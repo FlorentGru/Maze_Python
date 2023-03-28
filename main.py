0daf68190cc737dd16dceccbcbd6214b87c7c078
Florent Gru <florent.gru@epitech.eu>
2023-03-28 22:37:18 +0200

    End of the project, add the solver of maze

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from window import Window
 from cell import Cell
 from maze import Maze
+import sys
 
 def main():
     num_rows = 12
@@ -12,8 +13,15 @@ def main():
     cell_size_y = (screen_y - 2 * margin) / num_rows
     win = Window(screen_x, screen_y)
 
+    sys.setrecursionlimit(10000)
     maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
+    print("maze created")
+    is_solveable = maze.solve()
+    if not is_solveable:
+        print("Maze can not be solved")
+    else:
+        print("Maze solved")
     win.wait_for_close()
 
 
